4ef036759981aed295b1040d3bc31d583e6fface
Gabriel Paiva <gabriel.paiva0@icloud.com>
2025-09-03 15:54:11 -0300

    fix(TurnstileWidget): correct callback handling for turnstile widget
    Use useEffect to properly manage window callback function and cleanup

--- src/components/TurnstileWidget.tsx
+++ src/components/TurnstileWidget.tsx
@@ -1,14 +1,32 @@
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    onTurnstileCallback?: (token: string) => void;
+  }
+}
+
 type Props = {
   siteKey: string;
   onCallback: (token: string) => void;
 };
 
 export default function TurnstileWidget({ siteKey, onCallback }: Props) {
+  useEffect(() => {
+    window.onTurnstileCallback = (token: string) => {
+      onCallback(token);
+    };
+
+    return () => {
+      window.onTurnstileCallback = undefined;
+    };
+  }, [onCallback]);
+
   return (
     <div
       className="cf-turnstile"
-      data-callback="onCallback"
       data-sitekey={siteKey}
+      data-callback="onTurnstileCallback"
     ></div>
   );
 }
